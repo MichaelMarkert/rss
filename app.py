247005bde810203f7cdb109e78aca7b6607abbf4
MichaelMarkert <101104547+MichaelMarkert@users.noreply.github.com>
2025-02-05 10:57:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -111,3 +111,55 @@ feed = {
 
 with open("hf_blog.json", "w") as f:
     json.dump(feed, f)
+
+## Museumsbund Stellenportal
+
+BASE_URL = "https://www.museumsbund.de/stellenangebote/"
+page = requests.get(BASE_URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+h3s = soup.find_all("h3", "teaser__headline--job")
+papers = []
+
+def extract_abstraction(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    date = soup.find("h4", "content__sidebar-title").text
+    date_published = date.replace("Veröffentlicht am ","").lstrip()
+    site_text = soup.find("div","content__main entry-content")
+    site_text = site_text.get_text("\n",strip=True)
+    return site_text, date_published
+
+
+for h3 in h3s:
+    title = h3.text
+    a = h3.find("a")
+    link = a["href"]
+    url = link
+    whodate = h3.find_next("div", "teaser__excerpt teaser__text p-summary e-content").text.replace("\n","")
+    whodate = ' '.join(whodate.split())
+    print(whodate)
+    site_text, date_published = extract_abstraction(url)  
+    papers.append({"title": title, "url": url, "abstract": whodate + " | " + site_text, "date_published": date_published})
+
+feed = {
+    "version": "https://jsonfeed.org/version/1",
+    "title": "Museumsbund Stellenportal",
+    "home_page_url": BASE_URL,
+    "feed_url": "https://example.org/feed.json",
+    "items": 
+        [
+            {
+                "id": p["url"],
+                "title": p["title"].strip(),
+                "content_text": p["abstract"].strip(),
+                "url": p["url"],
+                "date_published": p["date_published"],
+            }
+            for p in papers
+        ],
+}
+
+with open("mb_jobs.json", "w") as f:
+    json.dump(feed, f)
