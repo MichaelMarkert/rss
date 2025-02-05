930ca7313868efdd776286086601611216668116
MichaelMarkert <101104547+MichaelMarkert@users.noreply.github.com>
2025-02-05 10:18:58 +0100

    Update app.py
    added hf_blog

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+## HF papers
+
 BASE_URL = "https://huggingface.co/papers"
 page = requests.get(BASE_URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 h3s = soup.find_all("h3")
 papers = []
-
 
 def extract_abstraction(url):
     page = requests.get(url)
@@ -24,7 +25,6 @@ def extract_abstraction(url):
         abstract = abstract[len("Abstract\n") :]
     abstract = abstract.replace("\n", " ")
     return abstract, datetime_str
-
 
 for h3 in h3s:
     a = h3.find("a")
@@ -61,4 +61,56 @@ feed = {
 }
 
 with open("hf_papers.json", "w") as f:
+    json.dump(feed, f)
+
+## HF Blog
+
+BASE_URL = "https://huggingface.co/blog"
+page = requests.get(BASE_URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+h2s = soup.find_all("h2")
+
+papers = []
+
+def extract_abstraction(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    date = soup.find("div", "mb-6 flex items-center gap-x-4 text-base")
+    if date:
+        span = date.find('span')
+        if span:
+            date = span.text.replace("Published\n","").lstrip()
+    return date
+
+
+for h2 in tqdm(h2s):
+    a = h2.find_parents("a",limit=1)[0]
+    title = h2.text
+    link = a["href"]
+    url = f"https://huggingface.co{link}"
+    date = extract_abstraction(url)  
+    papers.append({"title": title, "url": url, "abstract": abstract, "date_published": date})
+
+feed = {
+    "version": "https://jsonfeed.org/version/1",
+    "title": "Hugging Face Blog",
+    "home_page_url": BASE_URL,
+    "feed_url": "https://example.org/feed.json",
+    "items": 
+        [
+            {
+                "id": p["url"],
+                "title": p["title"].strip(),
+                "content_text": p["abstract"].strip(),
+                "url": p["url"],
+                "date_published": p["date_published"],
+            }
+            for p in papers
+        ],
+}
+
+
+with open("hf_blog.json", "w") as f:
     json.dump(feed, f)
